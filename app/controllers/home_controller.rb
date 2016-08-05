3ce05dfbd349a08bb04a29ed88359796ca05eda4
Claudia Farias <krawdyah@gmail.com>
2016-08-05 02:26:03 -0300

    categories page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,26 +2,25 @@ class HomeController < ApplicationController
   layout 'application'
   
   def index
-    # @categories = Category.masters.group_by { |cat| cat.name[0] }
     @services = Service.active.order(created_at: :desc).limit(10)
     render layout: 'home'
   end
 
   def categories
-    @categories = Category.search('*', where: { primary: true, published: true, services_total: { gt: 0 } }, order: 'name', per_page: 200)
+    @categories = Category.masters.group_by { |cat| cat.name[0] }
     @services = Services::Search.for('*', params[:sort], params[:page], { category_id: params[:id] })
   end
 
   def category
     @category = Category.includes(services: [:district]).by_slug(params[:id])
-    @services = Services::Search.for('*', params[:sort], params[:page], { category_id: params[:id] })
+    @category_ids = @category.primary ? @category.children.map(&:permalink) : params[:id]
+    @services = Services::Search.for('*', params[:sort], params[:page], { category_id: @category_ids })
   end
 
   def districts
     @city = params[:city_id] ? City.by_slug(params[:city_id]) : current_city
     # @districts = District.for({ city_id: @city.id })
     @districts = District.where(city_id: @city.id).order(:name).group_by { |cat| cat.name[0] }
-    @services = Services::Search.for('*', params[:sort], params[:page], { category_id: params[:id] })
   end
 
   def district
